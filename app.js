2c2a41658a5d3292658f6efd3a26b6e7d605eadf
Ciarroca Emiliano <ciarrocaemiliano@gmail.com>
2024-05-25 20:52:50 -0300

    Obtener categorias de API en seccion "contacto"

--- app.js
+++ app.js
@@ -1,5 +1,15 @@
-const options = {method: 'GET', headers: {accept: 'application/json'}};
-fetch('https://fakestoreapi.com/products', options)
-.then(response => response.json())
-.then(response => console.log(response))
-.catch(err => console.error(err));
+document.addEventListener("DOMContentLoaded", () => {
+    const categorySelect = document.getElementById('category');
+    
+    fetch('https://api.escuelajs.co/api/v1/categories', { method: 'GET', headers: { accept: 'application/json' } })
+        .then(response => response.json())
+        .then(categories => {
+            categories.forEach(category => {
+                const option = document.createElement('option');
+                option.value = category.id;
+                option.textContent = category.name;
+                categorySelect.appendChild(option);
+            });
+        })
+        .catch(err => console.error(err));
+});
